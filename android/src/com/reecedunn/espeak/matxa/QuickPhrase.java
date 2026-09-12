7752b93448ef2e40036339f87b7bf37f333721e2
davidfores-dev <davidforesvidal@gmail.com>
2026-09-13 00:58:36 +0200

    Add QuickPhrase class for label and text storage

---
+++ android/src/com/reecedunn/espeak/matxa/QuickPhrase.java
@@ -0,0 +1,11 @@
+package com.reecedunn.espeak.matxa;
+
+public class QuickPhrase {
+    public final String label;
+    public final String text;
+
+    public QuickPhrase(String label, String text) {
+        this.label = label;
+        this.text = text;
+    }
+}
